c2f181df5f7994426710b6c2c055ba0ae35a57aa
Raúl Angel Jiménez <Raulangel51@users.noreply.github.com>
2020-04-29 19:38:53 -0600

    f

--- programa.s
+++ programa.s
@@ -9,17 +9,21 @@ main:
 	/* valor1 */
 	
 	/*Bienvenida*/
-	ldr r1, =menu
-	ldr r0, =entradaS
+	ldr r0, =menu
 	bl printf
 	
 	/*pide opcion*/
-	ldr r0,=entradaC
+	ldr r0,=entradaS
 	ldr r1,=opcion
 	bl scanf
 	
+	/*Muestra la opcion elegida*/
+	ldr r1, =opcion
+	ldr r0, =opciong
+	bl printf
+	
 	/*compara para la opcione*/
-	ldrb r10, =opcion
+	/*ldr r10, =opcion
 	
 	cmp r10, #'+'
 	beq sumo
@@ -41,28 +45,32 @@ multip:
 	ldr r1, =mul
 	ldr r0, =entradaS
 	bl printf
-	b fin
+	b fin*/
 	
-fin:
-	mov r0, #0
-	mov r3,#0
-	ldmfd sp!,{lr}
-	bx lr
+	/* salida correcta */
+	fin:
+		@@ r0, r3 <- 0 como sennal de no error al sistema operativo
+		mov	r3, #0
+		mov	r0, r3
+
+		@ colocar registro de enlace para desactivar la pila y retorna al SO
+		ldmfd	sp!, {lr}
+		bx	lr
 	
 /*variables*/
 .data
 .align 2
-menu:			.asciz "Eliga una de estas opciones:\nIngrese '+' para Suma\nIngrese '*' para Multiplicación\nIngrese 'M' para Módulo (devolver el residuo en formato int de una división )\nIngrese 'P' para Potencia de un número (primer número ingresado es la base, segundo la potencia)\nIngrese '=' para Muestra el resultado almacenado\nIngrese 'q' para Mostrar mensaje de despedida y salir al sistema operativo"
-entradaS: 		.asciz "%s \n"
-entradaC:		.asciz "%c \n"
-opcion: 		.word 0
+menu:			.asciz "Eliga una de estas opciones:\nIngrese '+' para Suma\nIngrese '*' para Multiplicación\nIngrese 'M' para Módulo (devolver el residuo en formato int de una división )\nIngrese 'P' para Potencia de un número (primer número ingresado es la base, segundo la potencia)\nIngrese '=' para Muestra el resultado almacenado\nIngrese 'q' para Mostrar mensaje de despedida y salir al sistema operativo\n"
+entradaS: 		.asciz " %s \n"
+entradaC:		.asciz " %c \n"
+opcion: 		.asciz " "
 suma:			.asciz "Tu opcion es suma\n"
 mul:			.asciz "Tu opcion es multi\n"
 p:				.asciz "Tu opcion es potencia\n"
 mod:			.asciz "Tu opcion es modulo\n"
 igual:			.asciz "Tu opcion es igual\n"
 q:				.asciz "Tu opcion es salida\n"
-
+opciong: 	.asciz "Tu opcion fue: %s \n"
 
 
 
